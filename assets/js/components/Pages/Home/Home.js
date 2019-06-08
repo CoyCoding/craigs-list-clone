0455c9472f8fd216d01c304e2b4d8b9ec7411cf2
Matt Coy <fluffytoycoy@gmail.com>
2019-06-08 13:56:19 -0500

    removed now unneeded methods from Home

--- assets/js/components/Pages/Home/Home.js
+++ assets/js/components/Pages/Home/Home.js
@@ -14,50 +14,6 @@ export default class Home extends Component {
 		this.state = {};
 	}
 
-	loadCategories = () => {
-		const testArray = [1, 2, 3, 4, 5, 6, 7];
-		return testArray.map((category, index) => {
-			return (
-				<div className="category-box" key={index}>
-					<div className={'title'}>community</div>
-					<div className="link-box">
-						<a href="#" className="link">
-							community
-						</a>
-						<a href="#" className="link">
-							community
-						</a>
-						<a href="#" className="link">
-							community
-						</a>
-						<a href="#" className="link">
-							community
-						</a>
-						<a href="#" className="link">
-							community
-						</a>
-						<a href="#" className="link">
-							community
-						</a>
-						<a href="#" className="link">
-							community
-						</a>
-						<a href="#" className="link">
-							community
-						</a>
-					</div>
-				</div>
-			);
-		});
-	};
-
-	loadTags = () => {
-		const tags = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-		return tags.map((tag, index) => {
-			return <div key={index}>Macbook Pro</div>;
-		});
-	};
-
 	render() {
 		return (
 			<div id={'home-container'}>
