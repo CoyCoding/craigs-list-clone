c05883456c7b61b4c700fa96eca5e7bca0183da5
Matt Coy <fluffytoycoy@gmail.com>
2019-06-08 16:58:49 -0500

    added listing html

--- assets/js/components/Pages/ListingsPage/ListingsPage.js
+++ assets/js/components/Pages/ListingsPage/ListingsPage.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faClock } from '@fortawesome/fontawesome-free-solid';
+import PriceSelection from './Selections/PriceSelection';
 
 export default class ListingsPage extends Component {
 	constructor() {
@@ -16,8 +17,55 @@ export default class ListingsPage extends Component {
 		const { match, location, history } = this.props;
 		return (
 			<div id={'listings-container'}>
-				City: {match.params.city} Category: {match.params.category} Listing:{' '}
-				{match.params.listings}
+				<section className={'item-filters'}>
+					<PriceSelection />
+					<div className={'make form-group'}>
+						<lable>Make</lable>
+						<select name="make" className="make">
+							<option value="Audi">Audi</option>
+							<option value="BMW">BMW</option>
+						</select>
+					</div>
+					<div className={'model form-group'}>
+						<lable>model</lable>
+						<select name="model" className="model">
+							<option value="M3">M3</option>
+							<option value="M5">M5</option>
+						</select>
+					</div>
+					<div className="filter-btns">
+						<div className="update-btn">Update</div>
+						<div className="reset-btn">Reset</div>
+					</div>
+				</section>
+				<section className="item-list">
+					<section className="view-filters">
+						<select name="view-dropdown" className="view-type">
+							<option value="grid-view">Grid-View</option>
+							<option value="column-view">Column-View</option>
+						</select>
+						<select name="date-dropdown" className="sort-by-date">
+							<option value="newest">Newest</option>
+							<option value="oldest">Oldest</option>
+						</select>
+					</section>
+					<section className="item-grid">
+						<div className="item">
+							<div className="item-img">
+								<div>$Price</div>
+							</div>
+							<div className="info">
+								<div className="item-info">
+									<h6>Title/Description</h6>
+									<h7>Date Posted</h7>
+								</div>
+								<div className="favorite">
+									<FontAwesomeIcon icon="star" />
+								</div>
+							</div>
+						</div>
+					</section>
+				</section>
 			</div>
 		);
 	}
---
+++ assets/js/components/Pages/ListingsPage/Selections/PriceSelection.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// import update from 'immutability-helper';
+// var classNames = require('classnames');
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faClock } from '@fortawesome/fontawesome-free-solid';
+
+export default class PriceSelection extends Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div className={'price form-group'}>
+				<lable>Price</lable>
+				<select name="min-price" className="min-price">
+					<option value="0">0</option>
+				</select>
+				<select name="max-price" className="max-price">
+					<option value="1000">1000</option>
+				</select>
+			</div>
+		);
+	}
+}
